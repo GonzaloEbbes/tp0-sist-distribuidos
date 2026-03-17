b53a1181f46fbb8fe06a254cb9bbdac9c2ed3200
GonzaloEbbes <gebbes@fi.uba.ar>
2026-03-17 00:00:54 -0300

    primera version, falta handlear casos de error

---
+++ tools/composer.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+
+	yaml "gopkg.in/yaml.v3"
+)
+
+type AppConfig struct {
+	Name     string                   `yaml:"name"`
+	Services map[string]ServiceConfig `yaml:"services"`
+	Networks map[string]NetworkConfig `yaml:"networks"`
+}
+
+type ServiceConfig struct {
+	ContainerName string   `yaml:"container_name"`
+	Image         string   `yaml:"image"`
+	Entrypoint    string   `yaml:"entrypoint"`
+	Environment   []string `yaml:"environment"`
+	DependsOn     []string `yaml:"depends_on"`
+	Networks      []string `yaml:"networks"`
+}
+
+type NetworkConfig struct {
+	Ipam IpamConfig `yaml:"ipam"`
+}
+
+type IpamConfig struct {
+	Driver string         `yaml:"driver"`
+	Config []SubnetConfig `yaml:"config"`
+}
+
+type SubnetConfig struct {
+	Subnet string `yaml:"subnet"`
+}
+
+func main() {
+	args := os.Args
+	if len(args) < 3 {
+		//TODO:
+		// faltan parámetros
+		fmt.Println("Usage: composer <output_file> <client_count>")
+		return
+	}
+
+	outputFile := args[1]
+	clientCountStr := args[2]
+
+	clientCount, err := strconv.Atoi(clientCountStr)
+	if err != nil {
+		// TODO: manejar error de conversión
+		fmt.Printf("Invalid client count: %s\n", clientCountStr)
+		return
+	}
+	AppConfig := AppConfig{
+		Name: "tp0",
+		Networks: map[string]NetworkConfig{
+			"testing_net": {
+				Ipam: IpamConfig{
+					Driver: "default",
+					Config: []SubnetConfig{
+						{
+							Subnet: "172.25.125.0/24",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	addClientServices(&AppConfig, clientCount)
+
+	rawFileContent, err := generateRawFileContent(AppConfig)
+	if err != nil {
+		// TODO: manejar error
+		fmt.Printf("Error generating file content: %v\n", err)
+		return
+	}
+	err = writeFile(outputFile, rawFileContent)
+}
+
+func addClientServices(appConfig *AppConfig, clientCount int) {
+	services := make(map[string]ServiceConfig, clientCount+1) // +1 para el server
+	services["server"] = ServiceConfig{
+		ContainerName: "server",
+		Image:         "server:latest",
+		Entrypoint:    "python3 /main.py",
+		Environment: []string{
+			"PYTHONUNBUFFERED=1",
+			"LOGGING_LEVEL=DEBUG",
+		},
+		Networks: []string{"testing_net"},
+	}
+
+	for i := 1; i <= clientCount; i++ {
+		clientService := ServiceConfig{
+			ContainerName: fmt.Sprintf("client%d", i),
+			Image:         "client:latest",
+			Entrypoint:    "/client",
+			Environment: []string{
+				"CLI_ID=" + fmt.Sprintf("%d", i),
+				"CLI_LOG_LEVEL=DEBUG",
+			},
+			DependsOn: []string{"server"},
+			Networks:  []string{"testing_net"},
+		}
+		services[fmt.Sprintf("client%d", i)] = clientService
+	}
+
+	appConfig.Services = services
+}
+
+func generateRawFileContent(appConfig AppConfig) ([]byte, error) {
+	yamlContent, err := yaml.Marshal(appConfig)
+	if err != nil {
+		return nil, err
+	}
+	return yamlContent, nil
+}
+
+func writeFile(filename string, content []byte) error {
+	var perm os.FileMode = 0644 // TODO: explicar
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, perm)
+	if err != nil {
+		return err
+	}
+	bytes, err := f.Write(content)
+	if err != nil {
+		return err
+	}
+	if bytes != len(content) {
+		// TODO: delete file if write was incomplete, then return error
+		return errors.New("incomplete write")
+	}
+	if err = f.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
