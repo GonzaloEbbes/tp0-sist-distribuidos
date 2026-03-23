e1e9e3bbfe717e83f960d05fa026ee014420c080
GonzaloEbbes <gebbes@fi.uba.ar>
2026-03-23 19:58:42 -0300

    corrijo typo file

--- tools/composer.go
+++ tools/composer.go
@@ -96,7 +96,7 @@ func addServices(appConfig *AppConfig, clientCount int) {
 		Environment:   []string{},
 		Networks:      []string{"testing_net"},
 		Volumes:       []string{"./server/config.ini:/config.ini"},
-		DependsOn: nil,
+		DependsOn:     nil,
 	}
 
 	for i := 1; i <= clientCount; i++ {
@@ -113,7 +113,7 @@ func addServices(appConfig *AppConfig, clientCount int) {
 			Networks:  []string{"testing_net"},
 			Volumes: []string{
 				"./client/config.yaml:/config.yaml",
-				fmt.Sprintf("./.data/dataset/agency-%d.csv:%s", i, datasetPath),
+				fmt.Sprintf("./data/dataset/agency-%d.csv:%s", i, datasetPath),
 			},
 		}
 		services[fmt.Sprintf("client%d", i)] = clientService
