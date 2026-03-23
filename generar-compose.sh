8190a13b4369f1c59f0394247ef6941129464fc4
GonzaloEbbes <gebbes@fi.uba.ar>
2026-03-23 20:03:57 -0300

    modifico el zippeo para no utilizar /dataset

--- generar-compose.sh
+++ generar-compose.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eu
 
-DATASET_DIR=".data/dataset"
+DATASET_DIR=".data"
 DATASET_ZIP=".data/dataset.zip"
 
-if [ ! -d "$DATASET_DIR" ]; then
+if [ ! -f "$DATASET_DIR/agency-1.csv" ]; then
   mkdir -p "$DATASET_DIR"
   unzip -q "$DATASET_ZIP" -d "$DATASET_DIR"
 fi
